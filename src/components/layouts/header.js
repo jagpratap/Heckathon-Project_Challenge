a3e2cce33271de91e3680f64c1110e4a70e7264c
jagpratap <jagpratapchauhan@gmail.com>
2022-10-10 22:50:42 +0530

    Header padding refactoring to match design

--- src/components/layouts/header.js
+++ src/components/layouts/header.js
@@ -1,7 +1,7 @@
 const Header = () => (
   <header className="header">
     <div className="container">
-      <img src="./assets/images/svg_icons/dphi_brand.svg" alt="Brand_logo" className="header_brand" />
+      <img className="header_brand" src="./assets/images/svg_icons/dphi_brand.svg" alt="Brand_logo" />
     </div>
   </header>
 );
