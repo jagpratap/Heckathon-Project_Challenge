7026a94474f7eb36bfb6726f1c7798086b13f4b2
jagpratap <jagpratapchauhan@gmail.com>
2022-10-11 00:19:43 +0530

    Add Feature section

--- src/components/app/home/index.js
+++ src/components/app/home/index.js
@@ -25,6 +25,33 @@ const statistics = [
   },
 ];
 
+const features = [
+  {
+    id: 1,
+    logo: "./assets/images/svg_icons/carbon_notebook-reference.svg",
+    title: "Prove your skills",
+    desc: "Gain substantial experience by solving real-world problems and pit against others to come up with innovative solutions.",
+  },
+  {
+    id: 2,
+    logo: "./assets/images/svg_icons/Vector.svg",
+    title: "Learn from community",
+    desc: "One can look and analyze the solutions submitted by the other Data Scientists in the community and learn from them.",
+  },
+  {
+    id: 3,
+    logo: "./assets/images/svg_icons/Robot.svg",
+    title: "Challenge yourself",
+    desc: "There is nothing for you to lose by participating in a challenge. You can fail safe, learn out of the entire experience and bounce back harder.",
+  },
+  {
+    id: 4,
+    logo: "./assets/images/svg_icons/IdentificationCard.svg",
+    title: "Prove your skills",
+    desc: "You will stand out from the crowd if you do well in AI challenges, it not only helps you shine in the community but also earns rewards.",
+  },
+];
+
 const Home = () => (
   <main>
     {/* Hero Section */}
@@ -64,6 +91,29 @@ const Home = () => (
             }
             return <div className="vertical_strip" />;
           })}
+        </div>
+      </div>
+    </section>
+    {/* Features Section */}
+    <section className="features_section">
+      <div className="container">
+        <div className="section_content">
+          <h1 className="content_title">
+            Why Participate in
+            {" "}
+            <span>AI Challenges?</span>
+          </h1>
+          <div className="content_grid">
+            {features.map(({
+              id, logo, title, desc,
+            }) => (
+              <div key={id} className="grid_card">
+                <img className="card_image" src={logo} alt={title} />
+                <p className="title">{title}</p>
+                <p className="desc">{desc}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
