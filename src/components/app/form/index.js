13d36865c99839fa05f26c0e3035b0b974b382e8
jagpratap <jagpratapchauhan@gmail.com>
2022-10-13 23:47:34 +0530

    Refactor and Style form page

--- src/components/app/form/index.js
+++ src/components/app/form/index.js
@@ -2,56 +2,57 @@ import React from "react";
 
 const Form = () => (
   <section className="form_section">
-    <div className="section_heading">
+    <div className="section_header">
       <div className="container">
-        <h1>Challenge Details</h1>
+        <h1 className="heading">Challenge Details</h1>
       </div>
     </div>
     <div className="section_form">
       <div className="container">
         <div className="form_inputs">
           <div className="form_input">
-            <label htmlFor="name">
+            <label className="label" htmlFor="name">
               Challenge Name
               <input type="text" id="name" name="name" />
             </label>
           </div>
           <div className="form_input">
-            <label htmlFor="name">
+            <label className="label" htmlFor="name">
               Start Date
               <input type="datetime-local" id="startDate" name="startDate" />
             </label>
           </div>
           <div className="form_input">
-            <label htmlFor="name">
+            <label className="label" htmlFor="name">
               End Date
               <input type="datetime-local" id="endDate" name="endDate" />
             </label>
           </div>
         </div>
         <div className="form_textarea">
-          <label htmlFor="description">
+          <label className="label" htmlFor="description">
             Description
             <textarea id="description" name="description" />
           </label>
         </div>
         <div className="form_upload">
+          <p className="label">Image</p>
           <label htmlFor="upload">
-            Image
-            <input type="file" id="upload" name="upload" />
+            Upload
+            <input type="file" id="upload" name="upload" accept="image/png, image/jpeg" />
           </label>
         </div>
         <div className="form_select">
-          <label htmlFor="level">
+          <label className="label" htmlFor="level">
             Level Type
             <select id="level" name="level">
-              <option value="easy">Easy</option>
+              <option value="easy" selected>Easy</option>
               <option value="medium">Medium</option>
               <option value="hard">Hard</option>
             </select>
           </label>
         </div>
-        <button className="form_button" type="button">
+        <button className="form_action" type="button">
           Create Challenge
         </button>
       </div>
