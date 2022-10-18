42d5d19176687c877bbb761e1e96be1dc9271d0e
jagpratap <jagpratapchauhan@gmail.com>
2022-10-19 01:59:22 +0530

    Add Link form react router to home link

--- src/components/layouts/header.js
+++ src/components/layouts/header.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Header = () => (
   <header className="header">
     <div className="container">
-      <a href="/home">
+      <Link to="/home">
         <img className="header_brand" src="./assets/images/svg_icons/dphi_brand.svg" alt="Brand_logo" />
-      </a>
+      </Link>
     </div>
   </header>
 );
