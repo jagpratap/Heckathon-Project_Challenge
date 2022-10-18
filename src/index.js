741677c26c8c07d8e9ac2fc95deb691e501cf251
jagpratap <jagpratapchauhan@gmail.com>
2022-10-19 02:04:59 +0530

    Add focus styling to inputs and textarea

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+
 import UserProvider from "./context/userContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
